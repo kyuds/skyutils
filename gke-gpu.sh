e43f60a9656b314aa8732a76310d961b8c325844
kyuds <kyuds@everspin.co.kr>
2025-05-15 10:44:37 +0900

    remove node taints so that we can actually request nodes with gpus

--- gke-gpu.sh
+++ gke-gpu.sh
@@ -5,12 +5,11 @@ echo "Using cluster name: $NAME"
 
 gcloud container clusters create "$NAME" \
     --zone=us-central1-c \
-    --num-nodes=1 \
-    --machine-type=n1-highmem-8 \
+    --num-nodes=4 \
+    --machine-type=n1-highmem-16 \
     --accelerator type=nvidia-tesla-t4,count=1 \
     --enable-ip-alias \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
     --maintenance-window "03:00" \
     --node-labels=type=gpu \
-    --node-taints=type=gpu:NoSchedule \
     --verbosity=info
