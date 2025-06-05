5c409da77386858a0d9a6f5df032d92a842b87ca
Daniel Shin <kyuseung1016@gmail.com>
2025-06-05 21:56:01 +0900

    advanced api server setup

--- api.sh
+++ api.sh
@@ -10,15 +10,22 @@ RELEASE_NAME=skypilot
 WEB_USERNAME=skypilot
 WEB_PASSWORD=temp123
 AUTH_STRING=$(htpasswd -nb $WEB_USERNAME $WEB_PASSWORD)
+SECRET_NAME=apiserver-ssh-key
 
 case "$1" in
     setup)
+        kubectl create secret generic $SECRET_NAME \
+            --namespace $NAMESPACE \
+            --from-file=skytestkeypair.pem=/Users/kyuds/Desktop/skytestkeypair.pem
         helm repo add skypilot https://helm.skypilot.co
         helm repo update
         helm upgrade --install $RELEASE_NAME skypilot/skypilot-nightly --devel \
             --namespace $NAMESPACE \
             --create-namespace \
-            --set ingress.authCredentials=$AUTH_STRING
+            --set ingress.authCredentials=$AUTH_STRING \
+            --set apiService.image=kyuds/skypilot:test \
+            --set-file apiService.sshNodePools=/Users/kyuds/.sky/ssh_node_pools.yaml \
+            --set apiService.sshKeySecret=$SECRET_NAME
         ;;
     status)
         kubectl get pods --namespace $NAMESPACE -l app=${RELEASE_NAME}-api --watch
