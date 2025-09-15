49f1987a125a0c1fec9e05aa3645d032f92c4b03
kyuds <kyuseung1016@gmail.com>
2025-09-15 13:30:01 +0900

    tune yaml to increase time for autoscaling

--- gke-drain-autoscale.sh
+++ gke-drain-autoscale.sh
@@ -3,11 +3,10 @@
 # Need to match node-pool name in gke-autoscale.sh
 
 NAME="${1:-skytest}"
-NODE_POOL="${2:-pool16vcpu}"
 
 echo "Using cluster name: $NAME with autoscaling pool $NODE_POOL"
 
-gcloud container node-pools resize $NODE_POOL \
-    --cluster=$NAME \
+gcloud container clusters resize $NAME \
+    --node-pool=pool-16vcpu \
     --zone=us-central1-c \
     --num-nodes=0
